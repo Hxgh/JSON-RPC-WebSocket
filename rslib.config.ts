39c265311ad3551b397b4be38a7fef5efa785372
刘洪宇 <>
2025-04-15 15:33:53 +0800

    chore: 更新构建配置和忽略文件
    更新 `.gitignore` 和 `.npmignore` 文件以更好地管理依赖和构建输出。
    在 `package.json` 中添加 `build:npm` 脚本以简化 npm 发布流程。
    调整 `rslib.config.ts` 以支持 ES2023 语法并优化构建输出。

--- rslib.config.ts
+++ rslib.config.ts
@@ -4,8 +4,33 @@ export default defineConfig({
   lib: [
     {
       format: 'esm',
-      syntax: 'es2021',
+      syntax: 'es2023',
       dts: true,
     },
   ],
+  output: {
+    copy: [{ from: './public' }, { from: 'README.md' }],
+    minify: {
+      js: true,
+      css: false,
+      jsOptions: {
+        minimizerOptions: {
+          mangle: false,
+          // 启用压缩
+          minify: true,
+          compress: {
+            defaults: false,
+            unused: true,
+            dead_code: true,
+            // 避免 remoteEntry 的全局变量被 tree-shaking
+            toplevel: false,
+          },
+          format: {
+            comments: 'some',
+            preserve_annotations: true,
+          },
+        },
+      },
+    },
+  },
 });
